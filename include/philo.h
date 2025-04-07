d355c022ef29162a88d53b57e8724b8730676e9f
Christian Rodriguez <chrrodri@cbr15s6.42barcelona.com>
2025-04-07 23:44:46 +0200

    0407-2344 made functions  static

--- include/philo.h
+++ include/philo.h
@@ -11,72 +11,66 @@
 /* ************************************************************************** */
 
 #ifndef PHILO_H
-# define PHILO_H
+#define PHILO_H
 
 /* ************************************************************************** */
 /*                               Includes                                     */
 /* ************************************************************************** */
 
-# include <unistd.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <pthread.h>
-# include <sys/time.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <unistd.h>
 
 /* ************************************************************************** */
 /*                              Structures                                    */
 /* ************************************************************************** */
 
-typedef struct s_table	t_table;
+typedef struct s_table t_table;
 
-typedef struct s_philosophers
-{
-	int			id;
-	pthread_t	thread;
-	long		last_meal_time;
-	int			meals_eaten;
-	t_table		*table;
-}	t_philosophers;
+typedef struct s_philosophers {
+  int id;
+  pthread_t thread;
+  long last_meal_time;
+  int meals_eaten;
+  t_table *table;
+} t_philosophers;
 
-typedef struct s_table
-{
-	int				num_philosophers;
-	int				time_to_die;
-	int				time_to_eat;
-	int				time_to_sleep;
-	int num_meals;
-	pthread_mutex_t	*forks;
-	pthread_mutex_t	print_lock;
-	t_philosophers	*philosophers;
-}	t_table;
-
+typedef struct s_table {
+  int num_philosophers;
+  int time_to_die;
+  int time_to_eat;
+  int time_to_sleep;
+  int num_meals;
+  pthread_mutex_t *forks;
+  pthread_mutex_t print_lock;
+  t_philosophers *philosophers;
+} t_table;
 
 /* ************************************************************************** */
 /*                         Initiation & Validation                            */
 /* ************************************************************************** */
 
-int		init_simulation(t_table *table, int argc, char **argv);
+int init_simulation(t_table *table, int argc, char **argv);
 
-void	start_simulation(t_table *table);
+void *philosopher_routine(void *arg);
 
-void	*philosopher_routine(void *arg);
+long get_time_in_ms(void);
 
-long	get_time_in_ms(void);
+void ft_usleep(int milliseconds);
 
-void	ft_usleep(int milliseconds);
-
-void	print_action(t_philosophers *philo, const char *message);
+void print_action(t_philosophers *philo, const char *message);
 
 /* ************************************************************************** */
 /*                          Error Handling                                    */
 /* ************************************************************************** */
 
-void	fatal_error(const char *msg);
+void fatal_error(const char *msg);
 
 /* ************************************************************************** */
 /*                          Initialization                                    */
 /* ************************************************************************** */
-
 
 /* ************************************************************************** */
 /*                          Memory Management                                 */
--- src/core/main.c
+++ src/core/main.c
@@ -12,30 +12,27 @@
 
 #include "../../include/philo.h"
 
-static void	start_simulation(t_table *table)
-{
-	int	i;
+static void start_simulation(t_table *table) {
+  int i;
 
-	i = 0;
-	while (i < table->num_philosophers)
-	{
-		table->philosophers[i].id = i + 1;
-		table->philosophers[i].last_meal_time = get_time_ms();
-		table->philosophers[i].table = table;
-		pthread_create(&table->philosophers[i].thread, NULL, philosopher_routine, 
-				&table->philosophers[i]);
-	}
-	i = 0;
-	while (i < table->num_philosophers)
-		pthread_join(table->philosophers[i].thread, NULL);
+  i = 0;
+  while (i < table->num_philosophers) {
+    table->philosophers[i].id = i + 1;
+    table->philosophers[i].last_meal_time = get_time_in_ms();
+    table->philosophers[i].table = table;
+    pthread_create(&table->philosophers[i].thread, NULL, philosopher_routine,
+                   &table->philosophers[i]);
+  }
+  i = 0;
+  while (i < table->num_philosophers)
+    pthread_join(table->philosophers[i].thread, NULL);
 }
 
-int	main(int argc, char **argv)
-{
-	t_table table;
+int main(int argc, char **argv) {
+  t_table table;
 
-	if (init_simulation(&table, argc, argv))
-		return (1);
-	start_simulation(&table);
-	return (0);
+  if (init_simulation(&table, argc, argv))
+    return (1);
+  start_simulation(&table);
+  return (0);
 }
