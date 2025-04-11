f5671c0745c8ff54fb5266be7e3a984b1afb0f8f
Christian Rodriguez <chrrodri@cbr2s2.42barcelona.com>
2025-04-11 21:49:55 +0200

    0411-2148 Routine seems to be working correctly, need to fix issue that the meals dilimiter does not work properly

--- src/simulation/routine.c
+++ src/simulation/routine.c
@@ -28,58 +28,79 @@ static void *one_philo(t_philosophers *philo) {
   return (NULL);
 }
 
-// static void *check_(t_philosophers *philo)
+static int death_check(t_philosophers *philo) {
+  PM_LOCK(philo->table->death_lock);
+  if (philo->table->someone_died) {
+    PM_UNLOCK(philo->table->death_lock);
+    return (1);
+  }
+  PM_UNLOCK(philo->table->death_lock);
+  return (0);
+}
+
+static void handle_forking(t_philosophers *philo) {
+  if (philo->id % 2 == 0) {
+    PM_LOCK(philo->table->forks[philo->id % philo->table->num_philo]);
+    if (death_check(philo))
+      return;
+    print_action(philo, "has taken a fork");
+    PM_LOCK(philo->table->forks[philo->id - 1]);
+    if (death_check(philo))
+      return;
+    print_action(philo, "has taken a fork");
+  } else {
+    PM_LOCK(philo->table->forks[philo->id - 1]);
+    if (death_check(philo))
+      return;
+    print_action(philo, "has taken a fork");
+    if (death_check(philo))
+      return;
+    PM_LOCK(philo->table->forks[philo->id % philo->table->num_philo]);
+    if (death_check(philo))
+      return;
+    print_action(philo, "has taken a fork");
+  }
+}
+
+static void do_cycle(t_philosophers *philo) {
+  print_action(philo, "is eating");
+  if (death_check(philo))
+    return;
+  philo->last_meal_time = get_time_in_ms();
+  ft_usleep(philo->table->time_to_eat);
+  philo->meals_eaten++;
+  if (philo->table->max_meals > 0 &&
+      philo->meals_eaten == philo->table->max_meals) {
+    PM_LOCK(philo->table->fed_lock);
+    philo->table->total_fed++;
+    PM_UNLOCK(philo->table->fed_lock);
+    if (death_check(philo))
+      return;
+  }
+  PM_UNLOCK(philo->table->forks[philo->id - 1]);
+  PM_UNLOCK(philo->table->forks[philo->id % philo->table->num_philo]);
+  if (death_check(philo))
+    return;
+  print_action(philo, "is sleeping");
+  ft_usleep(philo->table->time_to_sleep);
+}
 
 void *philo_routine(void *arg) {
   t_philosophers *philo;
 
   philo = (t_philosophers *)arg;
+  // one philo edge case
   if (philo->table->num_philo == 1)
     return (one_philo(philo));
   while (1) {
-
-    PM_LOCK(philo->table->death_lock);
-    if (philo->table->someone_died) {
-      PM_UNLOCK(philo->table->death_lock);
-      break; // Exit thread if someone died
-    }
-    PM_UNLOCK(philo->table->death_lock);
-    print_action(philo, "is thinking");
-    // Taking forks safely (even/odd strategy)
-    if (philo->id % 2 == 0) {
-      // Even: Take right fork first
-      PM_LOCK(philo->table->forks[philo->id % philo->table->num_philo]);
-      print_action(philo, "has taken a fork");
-
-      PM_LOCK(philo->table->forks[philo->id - 1]);
-      print_action(philo, "has taken a fork");
-    } else {
-      // Odd: Take left fork first
-      PM_LOCK(philo->table->forks[philo->id - 1]);
-      print_action(philo, "has taken a fork");
-      PM_LOCK(philo->table->forks[philo->id % philo->table->num_philo]);
-      print_action(philo, "has taken a fork");
-    }
-    //  Eating
-    print_action(philo, "is eating");
-    philo->last_meal_time = get_time_in_ms();
-    ft_usleep(philo->table->time_to_eat);
-
-    // meals limit check
-    philo->meals_eaten++;
-    if (philo->table->max_meals > 0 &&
-        philo->meals_eaten == philo->table->max_meals) {
-      PM_LOCK(philo->table->fed_lock);
-      philo->table->total_fed++;
-      PM_UNLOCK(philo->table->fed_lock);
-    }
-    // Releasing forks
-    PM_UNLOCK(philo->table->forks[philo->id - 1]);
-    PM_UNLOCK(philo->table->forks[philo->id % philo->table->num_philo]);
-
-    // Sleeping
-    print_action(philo, "is sleeping");
-    ft_usleep(philo->table->time_to_sleep);
+    if (death_check(philo))
+      break;
+    handle_forking(philo);
+    if (death_check(philo))
+      break;
+    do_cycle(philo);
+    if (death_check(philo))
+      break;
   }
   return (NULL);
 }
